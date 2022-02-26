3b6b26ca2d9be6d68531c68910ab495357c1bff7
SAIKI <tuyo5kobe1011@gmail.com>
2022-02-26 13:00:49 +0000

    stopwatch

---
+++ main.js
@@ -0,0 +1,44 @@
+let display = document.getElementById("display")
+let start = document.getElementById("start")
+let stop = document.getElementById("stop")
+let reset = document.getElementById("reset")
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+let interval;
+let status = "stop";
+
+function stopWatch() {
+ seconds++;
+ if(seconds / 60==1) {
+   minutes++
+   seconds = 0;
+   
+   if(minutes / 60 == 1) {
+   hours++
+   minutes = 0;
+ } 
+ }
+ 
+display.innerHTML = hours+":"+minutes+":"+seconds;
+}
+
+start.addEventListener("click",function(){
+status == "stop"
+interval = setInterval(stopWatch,100)
+status = "start" ;
+})
+
+stop.addEventListener("click",function(){
+ clearInterval(interval)
+ status == "stop" ;
+})
+
+reset.addEventListener("click",function(){
+display.innerHTML = "00:00:00";
+hours:0;
+minutes:0;
+seconds:0;
+})
